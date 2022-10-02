980b86e8b9ef6f2384d29b6de99c129fd35ad965
Vikas <viki.badoni031@gmail.com>
2022-10-02 19:26:37 +0530

    Section3: 21

--- One/Cards/deck.go
+++ One/Cards/deck.go
@@ -2,10 +2,23 @@ package main
 
 import "fmt"
 
-type deck []string //defining a new type
+type deck []string 
 
-func (d deck) print() { // The receiver here is type deck,
-	for i, card := range d { //so this method can be called on any object with type deck
-		fmt.Println(i, card) // d is a reference to the cards from main.go
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts"}
+	cardValues := []string{"Ace", "Two", "Three"}
+
+	for _, suit := range cardSuits { 				//unusead vars can be replaced by _
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() { 
+	for i, card := range d { 
+		fmt.Println(i, card) 
 	}
 }
--- One/Cards/main.go
+++ One/Cards/main.go
@@ -1,11 +1,8 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()} 	//deck in defined as a string slice type in deck.go
-	cards = append(cards, "Four of Spades")
-
-	cards.print() 		//print method can be used on cards bcoz the receiver type for
-						//print method is deck
+	cards := newDeck()
+	cards.print()
 }
 
 func newCard() string {
