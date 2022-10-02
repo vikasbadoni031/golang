ffe6d18817b8a718bb1d4ced903c965e1339d476
Vikas <viki.badoni031@gmail.com>
2022-10-02 17:37:27 +0530

    Section3: 16

--- One/Cards/main.go
+++ One/Cards/main.go
@@ -2,8 +2,12 @@ package main
 
 import "fmt"
 
-func main() {  //any program having a package main must have a main func, which gets executed when the executable is executed
-	card := "Ace of spades" 
-	card = "Five of diamonds"  //:= is only used durin the first assigment
+func main() {
+	card := newCard()
+
 	fmt.Println(card)
+}
+
+func newCard() string { //defined the return type of the function
+	return "Five of Diamonds"
 }
