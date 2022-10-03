bde0588117d23df1a91b67b2434dc318748f3b30
Vikas <viki.badoni031@gmail.com>
2022-10-03 14:15:13 +0530

    Section3: 34,35

--- One/Cards/deck_test.go
+++ One/Cards/deck_test.go
@@ -7,6 +7,21 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Expected deck length of 16, but got %v", len(d))    //%v to inject a string
+		t.Errorf("Expected deck length of 16, but got %v", len(d)) //%v to inject a string
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
