a29da5b4de8a48adbd3d35ed2bcd6918eb3e59f4
Vikas <viki.badoni031@gmail.com>
2022-10-03 17:22:57 +0530

    Section5: 51

--- One/map/main.go
+++ One/map/main.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	//var colors map[string]string  //option1 to create map
 
-	colors := make(map[string]string) //option2 to create map
+	colors := map[string]string{ //first string for key and second one for values option3 to create map and initialize
+		"red":   "#ffakjsbd",
+		"green": "#asvbdkjasbd",
+		"white": "#kajsbd",
+	}
 
-	// colors := map[string]string{ //first string for key and second one for values option3 to create map and initialize
-	// 	"red":   "#ffakjsbd",
-	// 	"green": "#asvbdkjasbd",
-	// }
+	printMap(colors)
+}
 
-	colors["red"] = "#jkabsdjk" //adding element to the struct, maps use square brace syntax
-
-	delete(colors, "red")  		//deleting element from a map
-	
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
