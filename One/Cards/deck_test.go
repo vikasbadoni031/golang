181e6c4eb25b710eff5dfbcbfabfd7519f145f41
Vikas <viki.badoni031@gmail.com>
2022-10-03 13:51:57 +0530

    Section3: 32,33

---
+++ One/Cards/deck_test.go
@@ -0,0 +1,12 @@
+//deck_test.go file
+package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))    //%v to inject a string
+	}
+}
