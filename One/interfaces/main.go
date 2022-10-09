a6be0de92c77baf4bc50d5ffd8021ec34cd2b66d
Vikas <viki.badoni031@gmail.com>
2022-10-09 12:34:38 +0530

    Section6: 55

---
+++ One/interfaces/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	eb.getGreeting()
+	sb.getGreeting()
+
+	printGreeting(eb)
+	printGreeting(sb)
+
+}
+
+type bot interface {
+	getGreeting() string
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (eb englishBot) getGreeting() string {
+	return "Hello there!"
+}
+
+func (sb spanishBot) getGreeting() string {
+	return "Hola!"
+}
